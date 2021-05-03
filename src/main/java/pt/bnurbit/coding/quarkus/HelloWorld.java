39bb9845b50f64085aa7f516510d30e83affd2c5
bruno <bnurbit@gmail.com>
2021-05-03 20:05:51 +0100

    added .md file for notes testing the hello endpoint

--- src/main/java/pt/bnurbit/coding/quarkus/HelloWorld.java
+++ src/main/java/pt/bnurbit/coding/quarkus/HelloWorld.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 @Path("/hello")
 public class HelloWorld {
@@ -11,6 +12,6 @@ public class HelloWorld {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello RESTEasy " + UUID.randomUUID();
     }
 }
---
+++ src/main/java/pt/bnurbit/coding/quarkus/quarkus.md
@@ -0,0 +1,13 @@
+Speed-up server starting time - native
+
+Installed features:
+* cdi - contexts and dependency injection
+* resteasy
+
+Profile dev - live coding activated
+- Quarkus loads everything that was changed on demand
+
+Dependencies:
+* quarkus-arc       https://quarkus.io/blog/quarkus-dependency-injection/
+* quarkus-resteasy
+* quarkus-junit5
